d37efec4f44edec2872c46aafe503f6b5128fa29
malozaibi <ozaibitech@gmail.com>
2023-10-02 21:53:20 +0300

    Add Modal

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -11,11 +11,20 @@
       </RouterLink>
       <div class="flex gap-3 flex-1 justify-end">
         <i
+          @click="toggleModal"
           class="fa-solid fa-circle-info text-xl hover:text-w-secondary duration-150 cursor-pointer"
         ></i>
         <i
           class="fa-solid fa-plus text-xl hover:text-w-secondary duration-150 cursor-pointer"
         ></i>
+        <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
+          <div class="text-black prose">
+            <h1>Hi!</h1>
+            <p>
+              This is a weather app used to practice vuejs, tailwind and api.
+            </p>
+          </div>
+        </BaseModal>
       </div>
     </nav>
   </header>
@@ -23,4 +32,11 @@
 
 <script setup>
 import { RouterLink } from "vue-router";
+import BaseModal from "./BaseModal.vue";
+import { ref } from "vue";
+
+const modalActive = ref(null);
+const toggleModal = () => {
+  modalActive.value = !modalActive.value;
+};
 </script>
